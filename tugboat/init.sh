72caebca86afc2e36277092522b9af118a2a250a
Ben Chavet <ben.chavet@lullabot.com>
2016-10-21 14:28:30 -0500

    try using relative path in init.sh

--- tugboat/init.sh
+++ tugboat/init.sh
@@ -55,14 +55,15 @@ EOF
 
 # Make sure previews work when tugboat_url is set to subpath
 if [ "$TUGBOAT_URL" == "http://$TUGBOAT_DOMAIN/$TUGBOAT_TAG-$TUGBOAT_TOKEN" ] || [ "$TUGBOAT_URL" == "https://$TUGBOAT_DOMAIN/$TUGBOAT_TAG-$TUGBOAT_TOKEN" ]; then
+    set +x
     if [[ $TUGBOAT_IMAGE == apache* ]]; then
         rm /var/www/html
         mkdir /var/www/html
-        ln -s /var/lib/tugboat/docroot /var/www/html/$TUGBOAT_TAG-$TUGBOAT_TOKEN
+        ln -s `readlink -e ../docroot` /var/www/html/$TUGBOAT_TAG-$TUGBOAT_TOKEN
     fi
     if [[ $TUGBOAT_IMAGE == nginx* ]]; then
         rm /usr/share/nginx/html
         mkdir /usr/share/nginx/html
-        ln -s /var/lib/tugboat/docroot /usr/share/nginx/html/$TUGBOAT_TAG-$TUGBOAT_TOKEN
+        ln -s `readlink -e ../docroot` /usr/share/nginx/html/$TUGBOAT_TAG-$TUGBOAT_TOKEN
     fi
 fi
